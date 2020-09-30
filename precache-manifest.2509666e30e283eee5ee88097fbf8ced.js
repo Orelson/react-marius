4327efc71a8c6b7d8bb039a07472613c82718064
marius <angeorelsonlekpea@gmail.com>
2020-09-30 07:42:37 +0000

    Updates

---
+++ precache-manifest.2509666e30e283eee5ee88097fbf8ced.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2e213006a96ad2862a9a5e774527c3c",
+    "url": "/Orelson/react-marius/index.html"
+  },
+  {
+    "revision": "c1b46c123ba8a88b789c",
+    "url": "/Orelson/react-marius/static/css/main.328ab505.chunk.css"
+  },
+  {
+    "revision": "27f2216f5f5d77270b70",
+    "url": "/Orelson/react-marius/static/js/2.046cbce2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Orelson/react-marius/static/js/2.046cbce2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1b46c123ba8a88b789c",
+    "url": "/Orelson/react-marius/static/js/main.d076d6bd.chunk.js"
+  },
+  {
+    "revision": "f5743eb5fd18b4df6053",
+    "url": "/Orelson/react-marius/static/js/runtime-main.ecd49a29.js"
+  }
+]);
